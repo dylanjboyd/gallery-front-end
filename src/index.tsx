fa567afd5ab9783f8f16d56eb85ba89e67a7c3e3
Dylan Boyd <5061312+dylanjboyd@users.noreply.github.com>
2022-07-20 09:56:05 +1000

    build: add React Router

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import "./index.css";
 import { configureMockServer } from "./mockServer";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -14,7 +16,9 @@ if (process.env.NODE_ENV === "development") {
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
